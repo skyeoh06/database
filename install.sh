b04f5fe436f3957d2f65efd20dd2be6e1159fcb7
skyeoh06 <felyciayeoh@yahoo.com>
2020-06-15 12:28:01 +0000

    added phpmyadmin and example

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Prevent phpmyadmin from being added to github
+touch .gitignore
+echo "public/phpmyadmin" >> .gitignore
+
+mkdir public
+cd public
+git clone https://github.com/phpmyadmin/phpmyadmin.git
+cd phpmyadmin
+composer update --no-dev
+yarn install
+cp config.sample.inc.php config.inc.php
+# Remember to set AllowNoPassword (line 38 in config.inc.php) to false
